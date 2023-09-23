b6fc597893013030b0f378982a4314ec5e599357
unknown <2397272451@qq.com>
2023-09-23 09:44:27 +0800

    pro1a linkedlistdeque 实现

--- proj1a/LinkedListDeque/src/LinkedListDeque.java
+++ proj1a/LinkedListDeque/src/LinkedListDeque.java
@@ -1,98 +1,73 @@
 public class LinkedListDeque<T>{
-    class element{/*类定义*/
-        T label;
-        element pre;
-        element next;
-        private element(T label,element pre,element next){
-            this.label = label;
+    private class ItemNode {
+        private final T item;
+        private ItemNode pre;
+        private ItemNode next;
+
+        public ItemNode(T item, ItemNode pre, ItemNode next) {
+            this.item = item;
             this.pre = pre;
             this.next = next;
         }
     }
-    element last;
-    element first;
-    int length = 0;
-    public LinkedListDeque() {
-        last = new element(null, null, null);
-        first = new element(null, null, last);
-        last.pre = first;
+    private ItemNode sentinel;
+    private int size;
+    public LinkedListDeque(){
+        sentinel = new ItemNode(null,null,null);
+        sentinel.pre = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
     }
-
-
-
     public void addFirst(T item){
-        element x = new element(item,first,first.next);
-        first.next.pre = x;
-        first.next = x;
-        length += 1;
+        sentinel.next.pre = new ItemNode(item,sentinel,sentinel.next);
+        sentinel.next = sentinel.next.pre;
+        size += 1;
     }
     public void addLast(T item){
-        element x = new element(item,last.pre,last);
-        last.pre.next = x;
-        last.pre = x;
-        length += 1;
+        sentinel.pre = new ItemNode(item,sentinel.pre,sentinel);
+        sentinel.pre.pre.next = sentinel.pre;
+        size += 1;
     }
     public boolean isEmpty(){
-        return first.next == last;
+        return this.sentinel.next != sentinel;
     }
     public int size(){
-        return length;
+        return size;
     }
     public void printDeque(){
-        for(element i = first.next;i.next != last;i = i.next){
-            System.out.print(i.label+" ");
+        ItemNode p = sentinel.next;
+        for(int i=0;i<size;i++){
+            System.out.print(p.item+" ");
+            p = p.next;
         }
-        System.out.print(last.pre.label+"");
     }
     public T removeFirst(){
-        element x = first.next;
-        if(first.next != last) {
-            first.next.next.pre = first;
-            first.next = first.next.next;
-            return x.label;
-        }else{
-            return null;
-        }
+        ItemNode a = sentinel.next;
+        sentinel.next.next.pre = sentinel;
+        sentinel.next = sentinel.next.next;
+        return a.item;
     }
     public T removeLast(){
-        element x = last.pre;
-        if(first != last){
-            last.pre.pre.next = last;
-            last.pre = last.pre.pre;
-            return x.label;
-        }else{
-            return null;
-        }
+        ItemNode a = sentinel.pre;
+        sentinel.pre.pre.next = sentinel;
+        sentinel.pre = sentinel.pre.pre;
+        return a.item;
     }
     public T get(int index){
-        LinkedListDeque<T> x = this;
-        if(first.next == last){
+        if(index+1 > size()){
             return null;
         }
-        if(index != 0){
-            x.first =x.first.next;
-            return get(index-1);
-        }else{
-            return x.first.label;
+        ItemNode a = sentinel.next;
+        for(int i =0;i<index;i++){
+            a = a.next;
         }
-    }
-    public T getPecursive(int index){
-        LinkedListDeque<T> x = this;
-        while(index != 0){
-            x.first = x.first.next;
-            index -= 1;
-        }
-        return x.first.label;
+        return a.item;
     }
     public static void main(String[] args){
-        System.out.print("hello,world");
-        LinkedListDeque<String> a = new LinkedListDeque<String>();
-        a.addFirst("ab");
-        a.addLast("cd");
-        System.out.print(a.first.next.label);
-        System.out.print(a.last.pre.label);
-        System.out.print(a.isEmpty());
-        System.out.printf("%d",a.size());
-        a.printDeque();
+        LinkedListDeque<Integer> p = new LinkedListDeque<Integer>();
+        p.addFirst(1);
+        p.addFirst(2);
+        p.addLast(3);
+        p.printDeque();
     }
 }
